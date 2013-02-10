dc9952ab0328dc39b839521ecd641c5fece3181c
Plamen Levchev <plamen@berkeley.edu>
2013-02-10 13:31:17 -0800

    added unit tests

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+SUCCESS               =   1  # : a success
+ERR_BAD_CREDENTIALS   =  -1  # : (for login only) cannot find the user/password pair in the database
+ERR_USER_EXISTS       =  -2  # : (for add only) trying to add a user that already exists
+ERR_BAD_USERNAME      =  -3  # : (for add, or login) invalid user name (only empty string is invalid for now)
+ERR_BAD_PASSWORD      =  -4
+
 class UsersModel(models.Model):
 	user = models.CharField(max_length=128)
 	password = models.CharField(max_length=128)
@@ -17,13 +23,13 @@ class UsersModel(models.Model):
 		return -1
 	def add(self,user,password):
 		if not (len(user) > 0 and len(user) <= 128):
-			return -3
+			return ERR_BAD_USERNAME
 		if len(password) > 128:
-			return -4
+			return ERR_BAD_PASSWORD
 		
 		exists = UsersModel.objects.filter(user=user)
 		if exists and exists[0]:
-			return -2
+			return ERR_USER_EXISTS
 		
 		newuser = UsersModel(user=user,password=password,count=1)
 		newuser.save()
--- users/tests.py
+++ users/tests.py
@@ -6,11 +6,68 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from users.models import UsersModel
 
+SUCCESS               =   1  # : a success
+ERR_BAD_CREDENTIALS   =  -1  # : (for login only) cannot find the user/password pair in the database
+ERR_USER_EXISTS       =  -2  # : (for add only) trying to add a user that already exists
+ERR_BAD_USERNAME      =  -3  # : (for add, or login) invalid user name (only empty string is invalid for now)
+ERR_BAD_PASSWORD      =  -4
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class UsersUnitTest(TestCase):
+	def test_add_new(self):
+		u = UsersModel()
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,SUCCESS)
+	def test_add_duplicate(self):
+		u = UsersModel()
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,SUCCESS)
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,ERR_USER_EXISTS)
+	def test_add_two(self):
+		u = UsersModel()
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,SUCCESS)
+		ret = u.add("test1234","pass123")
+		self.assertEqual(ret,SUCCESS)
+	def test_add_empty(self):
+		u = UsersModel()
+		ret = u.add("","pass123")
+		self.assertEqual(ret,ERR_BAD_USERNAME)
+	def test_long_username(self):
+		u = UsersModel()
+		uname = "a"*129
+		ret = u.add(uname,"pass123")
+		self.assertEqual(ret,ERR_BAD_USERNAME)
+	def test_long_password(self):
+		u = UsersModel()
+		password = "a"*129
+		ret = u.add("test123",password)
+		self.assertEqual(ret,ERR_BAD_PASSWORD)
+	def test_login(self):
+		u = UsersModel()
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,SUCCESS)
+		ret = u.login("test123","pass123")
+		self.assertEqual(ret,2)
+	def test_wrongpassword(self):
+		u = UsersModel()
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,SUCCESS)
+		ret = u.login("test123","pass1234")
+		self.assertEqual(ret,ERR_BAD_CREDENTIALS)
+	def test_wronguser(self):
+		u = UsersModel()
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,SUCCESS)
+		ret = u.login("test1234","pass1234")
+		self.assertEqual(ret,ERR_BAD_CREDENTIALS)
+	def test_login2(self):
+		u = UsersModel()
+		ret = u.add("test123","pass123")
+		self.assertEqual(ret,SUCCESS)
+		ret = u.login("test123","pass123")
+		self.assertEqual(ret,2)
+		ret = u.login("test123","pass123")
+		self.assertEqual(ret,3)
